8e0c4b488edf565d703c746956b37abf31d7608f
Dushyant <117421735+Honey25609@users.noreply.github.com>
2024-10-04 00:09:39 +0530

    added cors

--- server.js
+++ server.js
@@ -13,11 +13,8 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json()); 
 
-app.use(cors({
-  origin: '*', 
-  methods: ['GET', 'POST'], 
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+const cors = require('cors');
+app.use(cors());
 
 
 
